0510355dfd7ae17ffdf4b437a9da0aa48aa785a2
Rafael <rafaelmagalhaes1@live.com>
2023-09-27 16:52:42 -0300

    styles: home page styled

--- src/pages/Home/Styles.tsx
+++ src/pages/Home/Styles.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Section = styled.section`
   display: flex;
-  background: darkcyan;
+  background: white;
   align-items: center;
   justify-content: center;
   height: 100vh;
@@ -11,8 +11,8 @@ export const Section = styled.section`
   .button-home {
     border: none;
     padding: 20px;
-    color: black;
-    background: white;
+    color: white;
+    background: #071952;
     font-size: 1rem;
     font-weight: 900;
     width: 20rem;
